4ba8c2e1c767d8ee815c0dd0402bf6284b71c421
NoceraSphen <stephen.nocera@richmondnational.com>
2025-04-04 09:31:49 -0400

    Added global cell positions

--- docling/backend/msexcel_backend.py
+++ docling/backend/msexcel_backend.py
@@ -169,7 +169,7 @@ class MsExcelDocumentBackend(DeclarativeDocumentBackend):
                     column_header=excel_cell.row == 0,
                     row_header=False,
                 )
-                table_data.table_cells.append((cell, cell.global_row, cell.global_col))
+                table_data.table_cells.append((cell, excel_cell.global_row, excel_cell.global_col))
 
             doc.add_table(data=table_data, parent=self.parents[0])
 
